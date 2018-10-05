7572d33bc90cfc787e8bed81595df4a4dfb64fc9
tuan11997 <201584000@student.hust.edu.vn>
2018-10-05 08:22:26 +0700

    Bài tập ss2

--- src/StopWatch/Watch.java
+++ src/StopWatch/Watch.java
@@ -1,14 +1,31 @@
 package src.StopWatch;
 
+import java.util.Scanner;
+
 public class Watch {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         setup StopWatch0 = new setup();
-        int count = 0;
-        StopWatch0.start();
-        for(int i = 0;i < 104440000;i++){
-            count += 12323;
-        }
-        StopWatch0.end();
-        System.out.println("It takes " + StopWatch0.getElapsedTime() + " milliseconds");
+        int choice;
+        do {
+            System.out.print("Press 1 to start and press 0 to get time ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Starting");
+                    int count = 0;
+                    StopWatch0.start();
+                    for (int i = 0; i < 104440000; i++) {
+                        count += 12323;
+                    }
+                case 0:
+                    StopWatch0.end();
+                    break;
+                default:
+                    System.out.println("Invaild input! ");
+
+            }
+        }while (choice != 0);
+        System.out.println("It takes " + (StopWatch0.getElapsedTime()/1000) + " seconds");
     }
 }
