2c58d37a9a3e94929b3bcb449767bbf114cfa9f6
tuan11997 <201584000@student.hust.edu.vn>
2018-10-05 11:22:28 +0700

    Bài tập ss3

--- src/LocationClass/Matrix.java
+++ src/LocationClass/Matrix.java
@@ -10,18 +10,18 @@ public class Matrix {
         System.out.println("Enter a number of collums");
         int Cols = scan.nextInt();
         createMatrix matrix = new createMatrix();
-        int[][] yourmatrix = matrix.createNewMatrix(Rows,Cols);
+        double[][] yourmatrix = matrix.createNewMatrix(Rows,Cols);
         enterData(scan,yourmatrix,matrix.Rows,matrix.Cols);
-        int[] result = matrix.locateLargest(yourmatrix);
+        double[] result = matrix.locateLargest(yourmatrix);
         System.out.println("Your largest number is " + result[2] +
-                " at row " + result[0] + " and column " + result[1] );
+                " at row " + (int)result[0] + " and column " + (int)result[1] );
     }
-    public static void enterData(Scanner scan,int[][] matrix, int Rows, int Cols){
+    public static void enterData(Scanner scan,double[][] matrix, int Rows, int Cols){
         System.out.println("Enter Matrix Data");
         for(int i = 0;i < Rows; i++){
             System.out.println("Row " + i);
             for (int j = 0; j < Cols; j++){
-                matrix[i][j] = scan.nextInt();
+                matrix[i][j] = scan.nextDouble();
             }
         }
     }
--- src/LocationClass/createMatrix.java
+++ src/LocationClass/createMatrix.java
@@ -6,16 +6,16 @@ public class createMatrix {
     public int Rows,Cols;
     public double Max;
 
-    public int[][] createNewMatrix(int Rows,int Cols){
+    public double[][] createNewMatrix(int Rows,int Cols){
         this.Rows = Rows;
         this.Cols = Cols;
-        int[][] matrix = new int[Rows][Cols];
+        double[][] matrix = new double[Rows][Cols];
         return matrix;
     };
 
-    public static int[] locateLargest(int[][] matrix){
-        int[] result = {1,1,0};
-        int max = matrix[0][0];
+    public static double[] locateLargest(double[][] matrix){
+        double[] result = {1,1,0};
+        double max = matrix[0][0];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
                 if (matrix[i][j] > max) {
